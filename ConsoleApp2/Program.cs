e27874205e133e3bd91367289066d8c858ebef70
Skykubo <kh44khm@yandex.ru>
2023-11-14 16:53:27 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{ 
+    class Program
+{ 
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину ребра куба a: ");
+            string tmp = Console.ReadLine();
+            int a = Int32.Parse(tmp);
+            int V = a * a * a;
+            int S = 6 * (a * a);
+            Console.WriteLine("Объем куба = " + V);
+                Console.WriteLine ("Площадь поверхности куба = " + S);
+
+    }
+}
+}
