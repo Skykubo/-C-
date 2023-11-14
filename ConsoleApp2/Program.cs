ecf1d79cd79fd30ce7cc1945e848e7afaf14c9d9
Skykubo <kh44khm@yandex.ru>
2023-11-14 17:55:30 +0700

    Begin10

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -28,13 +28,30 @@ namespace ConsoleApp2
             //Console.WriteLine("Площадь круга = " + S);
 
             //                              Begin9
+            //Console.WriteLine("Введите два числа: ");
+            //string tmp=Console.ReadLine();
+            //int a = Int32.Parse(tmp);
+            //string tm = Console.ReadLine();
+            //int b = Int32.Parse(tm);
+            //double c = Math.Sqrt(a * b);
+            //Console.WriteLine("Среднее геометрическое чисел = " + c);
+
+            //                      Begin10
             Console.WriteLine("Введите два числа: ");
             string tmp=Console.ReadLine();
-            int a = Int32.Parse(tmp);
-            string tm = Console.ReadLine();
-            int b = Int32.Parse(tm);
-            double c = Math.Sqrt(a * b);
-            Console.WriteLine("Среднее геометрическое чисел = " + c);
+            double a= Int32.Parse(tmp);
+            string tm=Console.ReadLine();
+            double b = Int32.Parse(tm);
+            double sum = (a*a)+(b*b);
+            double dif= (a*a)- (b*b);
+            double prod = (a*a)*(b*b);
+            double quo= (a*a)/(b*b);
+            Console.WriteLine("Сумма квадратов = " + sum);
+            Console.WriteLine("Разность квадратов = " + dif);
+            Console.WriteLine("Произведение квадратов = " + prod);
+            Console.WriteLine("Частное квадратов = " + quo);
+
+
         }
 }
 }
