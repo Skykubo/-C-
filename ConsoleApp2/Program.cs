c673081dcd8e03b8f53887b14d96ec24186bf1f2
Skykubo <kh44khm@yandex.ru>
2023-11-14 18:06:41 +0700

    IF2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -37,21 +37,35 @@ namespace ConsoleApp2
             //Console.WriteLine("Среднее геометрическое чисел = " + c);
 
             //                      Begin10
-            Console.WriteLine("Введите два числа: ");
+            //Console.WriteLine("Введите два числа: ");
+            //string tmp=Console.ReadLine();
+            //double a= Int32.Parse(tmp);
+            //string tm=Console.ReadLine();
+            //double b = Int32.Parse(tm);
+            //double sum = (a*a)+(b*b);
+            //double dif= (a*a)- (b*b);
+            //double prod = (a*a)*(b*b);
+            //double quo= (a*a)/(b*b);
+            //Console.WriteLine("Сумма квадратов = " + sum);
+            //Console.WriteLine("Разность квадратов = " + dif);
+            //Console.WriteLine("Произведение квадратов = " + prod);
+            //Console.WriteLine("Частное квадратов = " + quo);
+
+            //                  IF2
+            Console.WriteLine("Введите число: ");
             string tmp=Console.ReadLine();
-            double a= Int32.Parse(tmp);
-            string tm=Console.ReadLine();
-            double b = Int32.Parse(tm);
-            double sum = (a*a)+(b*b);
-            double dif= (a*a)- (b*b);
-            double prod = (a*a)*(b*b);
-            double quo= (a*a)/(b*b);
-            Console.WriteLine("Сумма квадратов = " + sum);
-            Console.WriteLine("Разность квадратов = " + dif);
-            Console.WriteLine("Произведение квадратов = " + prod);
-            Console.WriteLine("Частное квадратов = " + quo);
-
-
+            int a=Int32.Parse(tmp);
+            if (a >= 0)
+            {
+                int b = a + 1;
+                Console.WriteLine(+b);
+            }
+            else
+            {
+                int b = a - 2; 
+                Console.WriteLine(+b);
+            }
+            
         }
 }
 }
